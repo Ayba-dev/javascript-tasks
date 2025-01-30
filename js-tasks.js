6ab9c765a300a7905cb6de78dbb34b0bffe18f92
aibek <aibek@mail.com>
2025-01-30 23:18:52 +0600

    first commit

---
+++ js-tasks.js
@@ -0,0 +1,37 @@
+//1
+
+// function oddCount(n){
+//     return Math.floor((n - 1) / 2);
+// }
+//
+// console.log(oddCount(15))
+
+//2
+
+// function containsEnglish(str) {
+//     return str.toLowerCase().includes('english')
+// }
+//
+// console.log(containsEnglish('Endglish'));
+
+//3
+
+// function grabDoll(dolls) {
+//     let bag = [];
+//
+//     for (let i = 0; i < dolls.length; i++) {
+//         if (dolls[i] === 'Hello Kitty' || dolls[i] === 'Barbie doll') {
+//             bag.push(dolls[i]);
+//         }else {
+//             continue
+//         }
+//
+//         if (bag.length === 2) {
+//             break;
+//         }
+//     }
+//
+//     return bag;
+// }
+//
+// console.log(grabDoll(["Mickey Mouse", "Hello Kitty", "Hello Kitty"]))
